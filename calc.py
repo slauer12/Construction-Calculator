cb04e393e283db457f3b086cbcd611c6e30ce9c2
slauer12 <slauer12@yahoo.com>
2019-02-27 08:04:41 -0600

    Complete re-write.

--- calc.py
+++ calc.py
@@ -1,229 +1,154 @@
-# User input
-try:
-    fe1 = int(input('Enter feet: '))
-except ValueError:
-    fe1 = 0
-try:
-    in1 = int(input('Enter inches: '))
-except ValueError:
-    in1 = 0
-try:
-    num1 = int(input('Enter numerator: '))
-except ValueError:
-    num1 = 0
-try:
-    den1 = int(input('Enter denominator: '))
-except ValueError:
-    den1 = 0
-operator1 = input('Choose operator: ')
-if operator1 == '+' or operator1 == '-' or operator1 == '*' or operator1 == '/':
-    pass
-else:
-    print('Invalid operation. Please try again.')
-    operator1 = input('Choose operator: ')
-try:
-    fe2 = int(input('Enter feet: '))
-except ValueError:
-    fe2 = 0
-try:
-    in2 = int(input('Enter inches: '))
-except ValueError:
-    in2 = 0
-try:
-    num2 = int(input('Enter numerator: '))
-except ValueError:
-    num2 = 0
-try:
-    den2 = int(input('Enter denominator: '))
-except ValueError:
-    den2 = 0
-
+import re
 incha = 0
 inchb = 0
-inchc = 0
 totinch = 0
+print('Construction Calculator Version 1.1.0\nType "C" to clear.')
 
+regex = re.compile(r'(?P<feet>[0-9]+\')?\-?(?P<inches>[0-9]+")?')
+regexfrac = re.compile(r'(?P<feet>[0-9]+\')?\-?(?P<inches>[0-9]+ [0-9]+/[0-9]+")?')
+regexonlyfrac = re.compile(r'(?P<feet>[0-9]+\')?\-?(?P<inches>[0-9]+/[0-9]+")?')
+regexdec = re.compile(r'(?P<feet>[0-9]+.[0-9]+\')?\-?(?P<inches>[0-9]+.[0-9]+\")?')
 
-# Convert feet to inches with initial values
-def converter(fe1, in1, fe2, in2):
-    global incha
-    global inchb
-    if num1 == 0 or den1 == 0 and num2 == 0 or den2 == 0:
-        incha += ((fe1 * 12) + in1)
-        inchb += ((fe2 * 12) + in2)
-    elif num2 == 0 or den2 == 0:
-        incha += ((fe1 * 12) + in1 + (num1 / den1))
-        inchb += ((fe2 * 12) + in2)
-    elif num1 == 0 or den1 == 0:
-        incha += ((fe1 * 12) + in1)
-        inchb += ((fe2 * 12) + in2 + (num2 / den2))
+# Search regex for matches in feet
+def ftbreakdown(*args):
+  for arg in args:
+    if '.' not in arg:
+      search = regex.search(*args)
+      feetsearch = (search.group('feet'))
+      feet = int(feetsearch.split("'")[0]) if feetsearch is not None else 0
+      return feet
     else:
-        incha += ((fe1 * 12) + in1 + (num1 / den1))
-        inchb += ((fe2 * 12) + in2 + (num2 / den2))
+      # Searches for decimal feet.
+      search = regexdec.search(*args)
+      feetsearch = (search.group('feet'))
+      feet = float(feetsearch.split("'")[0]) if feetsearch is not None else 0
+      return feet
 
-# Convert secondary feet value to inches
-def converter2(fe, inc, num, den):
-    global inchc
-    if num == 0 or den == 0:
-        inchc += (fe * 12) + inc
+# Search regex for matches for inches and fractions of an inch
+def inbreakdown(*args):
+  for arg in args:
+    if '/' not in arg and '.' not in arg:
+      # Searches for inches only.
+      search = regex.search(*args)
+      inchsearch = (search.group('inches'))
+      inches = float(inchsearch.split('"')[0]) if inchsearch is not None else 0
+      return inches
+    elif '/' in arg and ' ' not in arg:
+      # Searches for fraction only.
+      search = regexonlyfrac.search(*args)
+      inchsearch = (search.group('inches'))
+      inches = 0
+      fracnum = int(inchsearch.split('/')[0])
+      fracden = int(inchsearch.split('/')[1].rstrip('"'))
+      frac = float(fracnum/fracden)
+      inches += frac
+      return inches
+    elif '.' in arg:
+      # Searches for decimal inches.
+      search = regexdec.search(*args)
+      inchsearch = (search.group('inches'))
+      inches = float(inchsearch.split('"')[0]) if inchsearch is not None else 0
+      return inches
     else:
-        inchc += ((fe * 12) + inc + (num / den))
+      # Searches for inches and fractions of an inch.
+      search = regexfrac.search(*args)
+      inchsearch = (search.group('inches'))
+      inches = int(inchsearch.split(' ')[0]) if inchsearch is not None else 0
+      fracstr = (inchsearch.split(' ')[1])
+      fracnum = int(fracstr.split('/')[0])
+      fracden = int(fracstr.split('/')[1].rstrip('"'))
+      frac = float(fracnum/fracden)
+      inches += frac
+      return inches
 
-def printer ():
-    # Prints out solution
-    global operator
-    global inchc
-    i = int(totinch)
-    j = (totinch - i) * 12
-    k = int(j)
-    frac1 = (j - k)
+# Printer extracts and prints out solution.
+def printer():
+  global length1
+  # tft returns the feet portion as an integer
+  tft = int(totinch / 12)
+  # tin returns the inch portion as an integer.
+  tin = ((totinch/12) - tft) * 12
+  # tinf and tfrac return the fraction portion.
+  tinf = int(tin)
+  tfrac = float(tin-tinf)
+  # Converts decimal to fraction for printing out.
+  if tfrac < .03125:
+    tfrac = ""
+  elif tfrac >= .03125 and tfrac < .09375:
+    tfrac = ' 1/16'
+  elif tfrac >= .09375 and tfrac < .15625:
+    tfrac = ' 1/8'
+  elif tfrac >= .15625 and tfrac < .21875:
+    tfrac = ' 3/16'
+  elif tfrac >= .21875 and tfrac < .28125:
+    tfrac = ' 1/4'
+  elif tfrac >= .28125 and tfrac < .34375:
+    tfrac = ' 5/16'
+  elif tfrac >= .34375 and tfrac < .40625:
+    tfrac = ' 3/8'
+  elif tfrac >= .40625 and tfrac < .46875:
+    tfrac = ' 7/16'
+  elif tfrac >= .46875 and tfrac < .53125:
+    tfrac = ' 1/2'
+  elif tfrac >= .53125 and tfrac < .59375:
+    tfrac = ' 9/16'
+  elif tfrac >= .59375 and tfrac < .65625:
+    tfrac = ' 5/8'
+  elif tfrac >= .65625 and tfrac < .71875:
+    tfrac = ' 11/16'
+  elif tfrac >= .71875 and tfrac < .78125:
+    tfrac = ' 3/4'
+  elif tfrac >= .78125 and tfrac < .84375:
+    tfrac = ' 13/16'
+  elif tfrac >= .84375 and tfrac < .90625:
+    tfrac = ' 7/8'
+  elif tfrac >= .90625 and tfrac < .96875:
+    tfrac = ' 15/16'
+  elif tfrac >= .96875:
+    tinf += 1
+    tfrac = ''
+  length1 = """{}'-{}{}" """.format(tft, tinf, tfrac)
+  print(length1)
+  looper()
 
-    # Fraction ranges to display (currently rounded to nearest 1/16th)
-    if frac1 > .015 and frac1 < .09374:
-        frac1 = ' 1/16'
-    elif frac1 > .09374 and frac1 < .15624:
-        frac1 = ' 1/8'
-    elif frac1 > .15624 and frac1 < .21874:
-        frac1 = ' 3/16'
-    elif frac1 > .21874 and frac1 < .28124:
-        frac1 = ' 1/4'
-    elif frac1 > .28124 and frac1 < .34374:
-        frac1 = ' 5/16'
-    elif frac1 > .34374 and frac1 < .40624:
-        frac1 = ' 3/8'
-    elif frac1 > .40624 and frac1 < .46874:
-        frac1 = ' 7/16'
-    elif frac1 > .46874 and frac1 < .53124:
-        frac1 = ' 1/2'
-    elif frac1 > .53124 and frac1 < .59374:
-        frac1 = ' 9/16'
-    elif frac1 > .59374 and frac1 < .65624:
-        frac1 = ' 5/8'
-    elif frac1 > .65624 and frac1 < .71874:
-        frac1 = ' 11/16'
-    elif frac1 > .71874 and frac1 < .78124:
-        frac1 = ' 3/4'
-    elif frac1 > .78124 and frac1 < .84374:
-        frac1 = ' 13/16'
-    elif frac1 > 0.84374 and frac1 < .90624:
-        frac1 = ' 7/8'
-    elif frac1 > .90624 and frac1 < .96874:
-        frac1 = ' 15/16'
-    elif frac1 > .96874:
-        k = int(round(j))
-        frac1 = ''
-    else:
-        frac1 = ''
+# Mathmatical functions.
+def functions():
+  global totinch
+  if operator == "+":
+    totinch = (incha + inchb)
+  elif operator == "-":
+    totinch = (incha - inchb)
+  elif operator == "*":
+    totinch = (incha * inchb)/12
+  elif operator == "/":
+    totinch = (incha / inchb)*12
+  printer()
 
-    print("""{}'-{}{}" """.format(i, k, frac1))
+# Converts lengths into readable format.
+def converter(*args):
+  global incha, inchb
+  ft1 = ftbreakdown(length1)
+  ft2 = ftbreakdown(length2)
+  in1 = inbreakdown(length1)
+  in2 = inbreakdown(length2)
+  incha = (ft1 * 12) + in1
+  inchb = (ft2 * 12) + in2
+  functions()
 
-    inchc = 0
-    operator = input('Choose operator: ')
-    if operator == '+' or operator == '-' or operator == '*' or operator == '/':
-        pass
-    else:
-        print('Invalid operation. Please try again.')
-        operator = input('Choose operator: ')
+def looper():
+  global operator, length2
+  operator = input('Choose operation: ')
+  if operator == 'c' or operator == 'C':
+    print('\n')
+    startup()
+  length2 = input('Enter length: ')
+  converter()
 
-# Initial addition function
-def add():
-    global totinch
-    totinch = (incha + inchb) / 12
-    printer()
+def startup():
+  global length1, length2, operator
+  length1 = input('Enter length: ')
+  operator = input('Choose operation: ')
+  length2 = input('Enter length: ')
+  converter()
 
-
-# Looping addition function
-def add2():
-    global totinch
-    global inchc
-    global operator
-    totinch += (inchc/12)
-    printer()
-
-# Initial subtraction function
-def subtract():
-    global totinch
-    totinch = (incha - inchb) / 12
-    printer()
-
-# Looping subtraction function
-def subtract2():
-    global totinch
-    global inchc
-    global operator
-    totinch -= (inchc/12)
-    printer()
-
-# Initial multiplication function
-def multiply():
-    global totinch
-    totinch = (incha * inchb) / 12
-    printer()
-
-# Looping multiplication function
-def multiply2():
-    global totinch
-    global inchc
-    global operator
-    totinch *= (inchc/12)
-    printer()
-
-# Initial division function
-def division():
-    global totinch
-    totinch = (incha / inchb) / 12
-    printer()
-
-# Looping division function
-def division2():
-    global totinch
-    global inchc
-    global operator
-    totinch /= (inchc/12)
-    printer()
-
-# Operator function selection
-if operator1 == '+':
-    converter(fe1, in1, fe2, in2)
-    add()
-
-elif operator1 == '-':
-    converter(fe1, in1, fe2, in2)
-    subtract()
-
-elif operator1 == '*':
-    converter(fe1, in1, fe2, in2)
-    multiply()
-
-elif operator1 == '/':
-    converter(fe1, in1, fe2, in2)
-    division()
-
-# Looping function directions
-while operator == '+' or operator == '-' or operator == '*' or operator == '/' or operator == 'cc':
-    try:
-        fe = int(input('Enter feet: '))
-    except ValueError:
-        fe = 0
-    try:
-        inc = int(input('Enter inches: '))
-    except ValueError:
-        inc = 0
-    try:
-        num = int(input('Enter numerator: '))
-    except ValueError:
-        num = 0
-    try:
-        den = int(input('Enter denominator: '))
-    except ValueError:
-        den = 0
-    converter2(fe, inc, num, den)
-    if operator == '+':
-        add2()
-    elif operator == '-':
-        subtract2()
-    elif operator == '*':
-        multiply2()
-    elif operator == '/':
-        division2()
-
+startup()
